047ce2e588110cd714e64594f9c080ddfa163c07
asareen2@charlotte.edu <asareen2@charlotte.edu>
2024-04-05 20:24:23 +0000

    Created ServerModule1, Form1 and LICENSE.txt; Edited settings and SaveAndSendEmail; Deleted HomeForm

---
+++ server_code/ServerModule1.py
@@ -0,0 +1,20 @@
+import anvil.users
+import anvil.email
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+@anvil.server.callable
+def say_hello(name):
+  print("Hello, " + name + "!")
+  app_tables.visitors.add_row(name=name, author=anvil.users.get_user())
+  return 42
+#
