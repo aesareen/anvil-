c0d118dee97b962d9edd2fffc44c0c568d3aa422
sindhugadi@gmail.com <sindhugadi@gmail.com>
2024-04-06 21:32:39 +0000

    Edited Form1

--- client_code/Form1.py
+++ client_code/Form1.py
@@ -18,6 +18,8 @@ class Form1(Form1Template):
     """This method is called when the button is clicked"""
     self.message_label.text = "Hello "  + self.name_box.text + "!"
     anvil.server.call('say_hello', self.name_box.text)
+    # Replace 'NewPage' with the name of the page you want to navigate to
+    self.open_form('ItemTemplate1')
 
   def text_box_1_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
