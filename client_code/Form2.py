228b9432a2b49f160c3d3b7a0e0539a1fd402227
rssalwekar@gmail.com <rssalwekar@gmail.com>
2024-04-07 05:19:09 +0000

    Created json_files/.gitkeep, json_files/courses.json, json_files/ratings_North Carolina State University.json, json_files/ratings_The University of North Carolina at Chapel Hill.json and 1 more; Edited Form2; Deleted json files/.gitkeep, json files/courses.json, json files/ratings_North Carolina State University.json, json files/ratings_The University of North Carolina at Chapel Hill.json and 1 more

--- client_code/Form2.py
+++ client_code/Form2.py
@@ -5,6 +5,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import json
 
 class Form2(Form2Template):
   def __init__(self, **properties):
@@ -12,6 +13,7 @@ class Form2(Form2Template):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+    with open("json")
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
