77a5f0ca52cd93f768d4b6c3d24633d504c0da25
rssalwekar@gmail.com <rssalwekar@gmail.com>
2024-04-07 04:40:04 +0000

    Created json files/ratings_North Carolina State University.json, json files/courses.json, json files/ratings_The University of North Carolina at Chapel Hill.json and json files/.gitkeep; Edited ItemTemplate1

--- client_code/ItemTemplate1/__init__.py
+++ client_code/ItemTemplate1/__init__.py
@@ -16,3 +16,7 @@ class ItemTemplate1(ItemTemplate1Template):
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('Form1')
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
